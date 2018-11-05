621cc4bd8319f724555632fbbdf0943e3e9c937b
chrispab <cbattisson@gmail.com>
2018-11-05 08:58:38 +0000

    tweak

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "version.h"
 #include "secret.h"
 
+#include "MyOTA.h"
 //#define SYS_FONT u8g2_font_8x13_tf
 #define SYS_FONT u8g2_font_6x12_tf       // 7 px high
 #define BIG_TEMP_FONT u8g2_font_fub30_tf //30px hieght
@@ -74,46 +75,48 @@ void setup()
     pinMode(ventPin, OUTPUT);
     pinMode(heaterPin, OUTPUT);
 
-    /* create a connection at port 3232 */
-    ArduinoOTA.setPort(3232);
-    /* we use mDNS instead of IP of ESP32 directly */
-    //hostname.local
-    ArduinoOTA.setHostname("esp32");
-    ArduinoOTA.setPassword("iotsharing");
+    setupOTA();
 
-    /* this callback function will be invoked when updating start */
-    ArduinoOTA.onStart([]() {
-        Serial.println("Start updating");
-    });
-    /* this callback function will be invoked when updating end */
-    ArduinoOTA.onEnd([]() {
-        Serial.println("\nEnd updating");
-    });
-    /* this callback function will be invoked when a number of chunks of software was flashed
-  so we can use it to calculate the progress of flashing */
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
+//     /* create a connection at port 3232 */
+//     ArduinoOTA.setPort(3232);
+//     /* we use mDNS instead of IP of ESP32 directly */
+//     //hostname.local
+//     ArduinoOTA.setHostname("esp32");
+//     ArduinoOTA.setPassword("iotsharing");
 
-    /* this callback function will be invoked when updating error */
-    ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-            Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-            Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-            Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-            Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR)
-            Serial.println("End Failed");
-    });
-    /* start updating */
-    ArduinoOTA.begin();
-    Serial.print("ESP IP address: ");
-    Serial.println(WiFi.localIP());
-    Serial.println(ArduinoOTA.getHostname());
+//     /* this callback function will be invoked when updating start */
+//     ArduinoOTA.onStart([]() {
+//         Serial.println("Start updating");
+//     });
+//     /* this callback function will be invoked when updating end */
+//     ArduinoOTA.onEnd([]() {
+//         Serial.println("\nEnd updating");
+//     });
+//     /* this callback function will be invoked when a number of chunks of software was flashed
+//   so we can use it to calculate the progress of flashing */
+//     ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+//         Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+//     });
+
+//     /* this callback function will be invoked when updating error */
+//     ArduinoOTA.onError([](ota_error_t error) {
+//         Serial.printf("Error[%u]: ", error);
+//         if (error == OTA_AUTH_ERROR)
+//             Serial.println("Auth Failed");
+//         else if (error == OTA_BEGIN_ERROR)
+//             Serial.println("Begin Failed");
+//         else if (error == OTA_CONNECT_ERROR)
+//             Serial.println("Connect Failed");
+//         else if (error == OTA_RECEIVE_ERROR)
+//             Serial.println("Receive Failed");
+//         else if (error == OTA_END_ERROR)
+//             Serial.println("End Failed");
+//     });
+//     /* start updating */
+//     ArduinoOTA.begin();
+//     Serial.print("ESP IP address: ");
+//     Serial.println(WiFi.localIP());
+//     Serial.println(ArduinoOTA.getHostname());
 
     // Send Email
     e.send("<cbattisson@gmail.com>", "<cbattisson@gmail.com>", "ESP32 zone controller start",
