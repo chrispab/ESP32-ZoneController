ff3f5fbb023526298f69f6be29de844f8ce1d6ab
chrispab <cbattisson@gmail.com>
2018-12-16 11:16:19 +0000

    port #2

--- src/main.cpp
+++ src/main.cpp
@@ -77,50 +77,9 @@ void setup()
 
     setupOTA();
 
-//     /* create a connection at port 3232 */
-//     ArduinoOTA.setPort(3232);
-//     /* we use mDNS instead of IP of ESP32 directly */
-//     //hostname.local
-//     ArduinoOTA.setHostname("esp32");
-//     ArduinoOTA.setPassword("iotsharing");
-
-//     /* this callback function will be invoked when updating start */
-//     ArduinoOTA.onStart([]() {
-//         Serial.println("Start updating");
-//     });
-//     /* this callback function will be invoked when updating end */
-//     ArduinoOTA.onEnd([]() {
-//         Serial.println("\nEnd updating");
-//     });
-//     /* this callback function will be invoked when a number of chunks of software was flashed
-//   so we can use it to calculate the progress of flashing */
-//     ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-//         Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-//     });
-
-//     /* this callback function will be invoked when updating error */
-//     ArduinoOTA.onError([](ota_error_t error) {
-//         Serial.printf("Error[%u]: ", error);
-//         if (error == OTA_AUTH_ERROR)
-//             Serial.println("Auth Failed");
-//         else if (error == OTA_BEGIN_ERROR)
-//             Serial.println("Begin Failed");
-//         else if (error == OTA_CONNECT_ERROR)
-//             Serial.println("Connect Failed");
-//         else if (error == OTA_RECEIVE_ERROR)
-//             Serial.println("Receive Failed");
-//         else if (error == OTA_END_ERROR)
-//             Serial.println("End Failed");
-//     });
-//     /* start updating */
-//     ArduinoOTA.begin();
-//     Serial.print("ESP IP address: ");
-//     Serial.println(WiFi.localIP());
-//     Serial.println(ArduinoOTA.getHostname());
-
     // Send Email
     e.send("<cbattisson@gmail.com>", "<cbattisson@gmail.com>", "ESP32 zone controller start",
-           "zone controller  started/restarted");
+           "ESP32 zone controller  started/restarted");
 
     myDisplay.begin();
     myDisplay.setFont(SYS_FONT);
@@ -175,14 +134,8 @@ void loop()
     //read sensors
     //get latest I/O states
     float targetTemp;
-    if (myLight.getLightState())
-    {
-        targetTemp = TSP_LON;
-    }
-    else
-    {
-        targetTemp = TSP_LOFF;
-    }
+    myLight.getLightState() ? targetTemp = TSP_LON : targetTemp = TSP_LOFF;
+    
 
     myVent.control(temperature, targetTemp, lightState, currentMillis);
     digitalWrite(ventPin, myVent.getState());
